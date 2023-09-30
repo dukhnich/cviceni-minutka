c75208d1d8fab7f0ba0c4bba432cbcdc3a51dfc0
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-09-30 13:49:28 +0200

    add alarm functionality

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('funguju!');
+const alarmEl = document.querySelector('.alarm');
+let seconds = Number(prompt('Kolik vteřin má uplynout než minutka začne zvonit?')) || 5;
+if (alarmEl) {
+    const timeoutId = setTimeout(() => {
+        alarmEl.classList.add('alarm--ring');
+        const audio = document.querySelector('audio');
+        if (audio) {
+            audio.play();
+        }
+    }, seconds * 1000);
+    const btn = document.querySelector('.btn--cancel');
+    if (btn) {
+        btn.addEventListener('click', () => {
+            clearTimeout(timeoutId);
+        });
+    }
+}
